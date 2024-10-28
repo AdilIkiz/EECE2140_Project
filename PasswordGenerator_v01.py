221b5a9d240835d23df1de7730e87edca9b96735
ialss <klaidaswik@gmail.com>
2024-10-28 12:33:53 -0400

    Rehashed Program

--- PasswordGenerator_v01.py
+++ PasswordGenerator_v01.py
@@ -5,59 +5,63 @@ import random
 
 print("Answer 'True' or 'False' based on whether you want or not")
 
-digits_bool = bool(input("Digits? "))
 letters_bool = bool(input("Letters? "))
 special_bool = bool(input("Special Characters? "))
 numbers_bool = bool(input("Numbers? "))
 
 
-def inititalization():
-	PWlength = int(input("Enter password length: "))
-	
+def generate_password():
 
-print('''Choose character set for password from these : 
-		1. Digits
-		2. Letters
-		3. Special characters
-		4. Exit''')
+	length = int(input("Enter password length: "))
 
-characterList = ""
+	password = []
+	alphabetChar = "abcdefghijklmnopqrstuvwxyz"
+	specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
-# Getting character set for password
-while(True):
-	choice = int(input("Pick a number "))
-	if(choice == 1):
-		
-		# Adding letters to possible characters
-		characterList += string.digits
-	elif(choice == 2):
-		
-		# Adding digits to possible characters
-		characterList += string.ascii_letters
-	elif(choice == 3):
-		
-		# Adding special characters to possible
-		# characters
-		characterList += string.punctuation
-	elif(choice == 4):
-		break
-	else:
-		print("Please pick a valid option!")
 
-password = []
 
-for i in range(length):
+	for i in range(length):
 
-	# Picking a random character from our 
-	# character list
-	randomchar = random.choice(characterList)
-	
-	# appending a random character to password
-	password.append(randomchar)
+		num_iterator = random.randint(1, 3)
 
-# printing password as a string
-print("The random password is " + "".join(password))
+		if(numbers_bool & (num_iterator == 1)):
+			password.append(str(random.randint(0,9)))
 
-#klaidas
+		elif(letters_bool & (num_iterator == 2)):
+			password.append(random.choice(alphabetChar))
+
+		elif(special_bool & (num_iterator == 3)):	
+			password.append(random.choice(specialChar))
+		else:
+			print("Something went wrong. Did you give valid inputs?")
+
+	print("".join(password))
+
+
+
+generate_password()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
