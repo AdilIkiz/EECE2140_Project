767f3cda1344b1a51024bfec6a61d2e04989781a
ialss <klaidaswik@gmail.com>
2024-10-28 13:41:01 -0400

    REDID THE WHOLE CODE WORKS NOW ! :D

--- PasswordGenerator_v01.py
+++ PasswordGenerator_v01.py
@@ -4,9 +4,6 @@ import random
 # Getting password length and character selection
 
 def user_preferences():
-	letters_bool = False
-	special_bool = False
-	numbers_bool = False
 
 	print("Answer '1' or '0' based on whether you want or not. 1 being yes and 0 being no")
 
@@ -26,27 +23,21 @@ def generate_password():
 	password = []
 	alphabetChar = "abcdefghijklmnopqrstuvwxyz"
 	specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-
-	while(True):
-
-		for i in range(length):
-
-			num_iterator = random.randint(1, 3)
-
-			if(numbers_bool & (num_iterator == 1)):
-				password.append(str(random.randint(0,9)))
-			elif(letters_bool & (num_iterator == 2)):
-				password.append(random.choice(alphabetChar))
-
-			elif(special_bool & (num_iterator == 3)):	
-				password.append(random.choice(specialChar))
-			else:
-				print("Something went wrong. Did you give valid inputs?")
-				letters_bool, special_bool, numbers_bool = user_preferences()
-				password.clear()
-				break
-		else:
-			break
+	numChar = "1234567890"
+	validChars = ""
+	
+	for i in range(3):
+		if(letters_bool):
+			validChars = validChars + alphabetChar
+			letters_bool = False
+		elif(special_bool):
+			validChars = validChars + specialChar
+			special_bool = False
+		elif(numbers_bool):
+			validChars = validChars + numChar
+			numbers_bool = False
+	for i in range(length):
+		password.append(random.choice(validChars))
 
 	print("".join(password))
 
